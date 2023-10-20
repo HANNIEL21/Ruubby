14caaf37414cf0f294ae3426bdaf4853367ed41a
HANNIEL21 <dhanniel21@gmail.com>
2023-10-20 06:03:51 +0100

    added persistor

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,14 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { authSlice } from "./Features/Auth/AuthSlice";
 import { userSlice } from "./Features/User/UserSlice";
 import { dashboardSlice } from "./Features/Dashboard/DashboardSlice";
 
+const rootReducer = combineReducers({
+    auth: authSlice.reducer,
+    user: userSlice.reducer,
+    dashboard: dashboardSlice.reducer,
+})
 
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        auth: authSlice.reducer,
-        user: userSlice.reducer,
-        dashboard: dashboardSlice.reducer,
-    }
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './Redux/Store';
+import { persistor, store } from './Redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
